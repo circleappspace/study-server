56294bc3d53e833fbcb7e14a5ed714c9ef918eb4
Nalgae9403 <wing9403@gmail.com>
2025-09-25 01:54:29 +0000

    엔드포인트 추가

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,34 @@
 import express from 'express'
+import { readFileSync, writeFileSync } from 'fs';
+
+function loadData() {
+  const raw = readFileSync('./database.json', 'utf-8');
+  const obj = JSON.parse(raw);
+  return obj;
+}
+
+function saveData(obj) {
+  const raw = JSON.stringify(obj, null, 2);
+  writeFileSync('./database.json', raw, 'utf-8');
+}
 
 const app = express()
+const bubbles = loadData()
 
 app.get('/', (req, res) => {
   res.send('index page')
 })
 
-app.get('/nalgae', (req, res) => {
-  res.send('nalgae')
+app.get('/api/v1/bubble', (req, res) => {
+  res.send(bubbles)
 })
 
-app.listen(80)
+app.get('/api/v1/bubble/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const bubble = bubbles.find(b => b.id === id)
+  res.send(bubble)
+})
+
+app.get('/api/v1/bubble/:id', (req, res) => {})
+
+app.listen(3000)
